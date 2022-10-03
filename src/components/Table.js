47b0db337482780f77f79c237bb71e664a26d0aa
Daniel Wolter <wolter.daniel@gmail.com>
2022-10-03 18:49:17 -0300

    req 6 e 7 ok

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,11 +1,116 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { Redirect } from 'react-router-dom';
 
 class Table extends Component {
+  state = {
+    redirect: false,
+  };
+
   render() {
+    const { redirect } = this.state;
+    const { expenses } = this.props;
     return (
-      <div>Table</div>
+      <div>
+        {expenses.length === 0
+          ? (
+            <table>
+              <tr>
+                <th>Descrição</th>
+                <th>Tag</th>
+                <th>Método de pagamento</th>
+                <th>Valor</th>
+                <th>Moeda</th>
+                <th>Câmbio utilizado</th>
+                <th>Valor convertido</th>
+                <th>Moeda de conversão</th>
+                <th>Editar/Excluir</th>
+              </tr>
+            </table>
+          )
+          : (
+            <table>
+              <thead>
+                <tr>
+                  <th>Descrição</th>
+                  <th>Tag</th>
+                  <th>Método de pagamento</th>
+                  <th>Valor</th>
+                  <th>Moeda</th>
+                  <th>Câmbio utilizado</th>
+                  <th>Valor convertido</th>
+                  <th>Moeda de conversão</th>
+                  <th>Editar/Excluir</th>
+                </tr>
+              </thead>
+              <tbody>
+                { expenses.length > 0 && (expenses.map((elem) => (
+                  <tr key={ `${elem.id}` }>
+                    <td>{elem.description}</td>
+                    <td>{elem.tag}</td>
+                    <td>{elem.method}</td>
+                    <td>{Number(elem.value).toFixed(2)}</td>
+                    <td>
+                      {
+                        `${elem.exchangeRates[elem
+                          .currency].name.split('/')[0]}${'/Real Brasileiro'}`
+                      }
+                    </td>
+                    <td>
+                      {
+                        (Number(elem.exchangeRates[elem.currency].ask).toFixed(2))
+                      }
+
+                    </td>
+                    <td>
+                      {
+                        (Number(elem.value)
+                          * Number(elem.exchangeRates[elem.currency].ask))
+                          .toFixed(2)
+                      }
+                    </td>
+                    <td>Real</td>
+                    <td>
+                      <button
+                        type="button"
+                        data-testid="edit-btn"
+                        onClick={ () => {
+                          receiveEdit(elem.id);
+                          this.setState({ redirect: true });
+                        } }
+                      >
+                        Editar
+                      </button>
+                      <button
+                        type="button"
+                        data-testid="delete-btn"
+                        onClick={ () => removeExpense(elem.id) }
+                      >
+                        Excluir
+                      </button>
+                    </td>
+                  </tr>
+                )))}
+              </tbody>
+            </table>
+          )}
+        { redirect && <Redirect to="/carteira" /> }
+      </div>
     );
   }
 }
 
-export default Table;
+const mapStateToProps = ({ wallet }) => ({
+  expenses: wallet.expenses,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  removeExpense: (id) => dispatch(removeAction(id)),
+});
+
+Table.propTypes = {
+  expenses: PropTypes.instanceOf(Array).isRequired,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Table);
